883e9adb1b9fb0e0669865be7a8398ab40e86e3b
Christopher Benavides <realchrisb@icloud.com>
2016-05-26 10:17:03 -0500

    Initial Commit

---
+++ Calculatron/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculatron
+//
+//  Created by Christopher Benavides on 5/26/16.
+//  Copyright © 2016 Christopher Benavides. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
